b90db150aba293800c32963456b11352fe4c420b
zaskhasasmita <zaskhasasmita22@gmail.com>
2024-02-22 10:51:36 +0700

    Feat : Add Routing for this simple app

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,22 @@
 <?php
 
-class App {
-    public function __construct() {
-        echo "App OK!";
+class App 
+{
+    public function __construct() 
+    {
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    // mengambil url lalu memecah sesuai keinginan kita 
+    public function parseURL() 
+    {
+        if( isset($_GET['url']) )
+        {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
